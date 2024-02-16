2f44c7ee92f2f624bc3d2fd3482268fdbed38410
Ricardo Lima <ricardolima.dev@gmail.com>
2024-02-15 22:59:57 -0300

    feature/005 constants adjust

--- src/main/java/br/com/api/pitang/constants/MessagesConstants.java
+++ src/main/java/br/com/api/pitang/constants/MessagesConstants.java
@@ -8,11 +8,10 @@ public class MessagesConstants {
     public static final String LOGIN_ALREADY_EXISTS = "Login already exists";
     public static final String INVALID_LOGIN = "Invalid login or password";
     public static final String UNAUTHORIZED = "Unauthorized";
-    public static final String INVALID_SESSION = "Unauthorized - invalid session";
     public static final String LICENSE_ALREADY_EXISTS = "License plate already exists";
     public static final String USER_NOT_FOUND = "User not found";
     public static final String CAR_NOT_FOUND = "Car not found";
-    public static final String INVALID_TOKEN = "Invalid Token";
+    public static final String INVALID_TOKEN = "Unauthorized - invalid session";
     public static final String PERMISSION_DENIED = "Unauthorized - permission denied";
 
 
